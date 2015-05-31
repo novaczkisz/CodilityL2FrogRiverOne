5176e1ddd217a2eb31c3cddcc46c00ab6c9f768e
Szabolcs Nováczki <szabolcs.novaczki@nsn.com>
2015-05-31 16:00:08 +0200

    solution_1
    Result: C80%; P60%; S72%

---
+++ FrogRiverOne.cpp
@@ -0,0 +1,23 @@
+int solution(int X, vector<int> &A) {
+
+    int N = A.size();
+
+    if( N < X ) return -1;
+
+    vector<int> counts(X,0);
+
+    for( int i = 0; i < N; i++ ){
+        if( counts[ A[i] -1 ] == 0 )
+            counts[ A[i] -1 ] = i;
+    }
+
+    int min_cross_time = 0;
+
+    for( int i = 0; i < X; i++ ){
+        if( counts[i] == 0 ) return -1;
+        if( counts[i] > min_cross_time ) min_cross_time = counts[i];
+    }
+
+    return min_cross_time;
+
+}
