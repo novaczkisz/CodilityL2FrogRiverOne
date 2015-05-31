209504a41ca8566219c6467dbe2d172a175e7777
Szabolcs Nováczki <szabolcs.novaczki@nsn.com>
2015-05-31 16:17:02 +0200

    solution_2
    C100; P100; S100

--- FrogRiverOne.cpp
+++ FrogRiverOne.cpp
@@ -4,17 +4,17 @@ int solution(int X, vector<int> &A) {
 
     if( N < X ) return -1;
 
-    vector<int> counts(X,0);
+    vector<int> counts(X,-1);
 
     for( int i = 0; i < N; i++ ){
-        if( counts[ A[i] -1 ] == 0 )
+        if( counts[ A[i] -1 ] == -1 )
             counts[ A[i] -1 ] = i;
     }
 
     int min_cross_time = 0;
 
     for( int i = 0; i < X; i++ ){
-        if( counts[i] == 0 ) return -1;
+        if( counts[i] == -1 ) return -1;
         if( counts[i] > min_cross_time ) min_cross_time = counts[i];
     }
 
